36ee022232d03518240da176ba2147debdbf30fc
Yohsuke Murase <flyingcamelspecial@gmail.com>
2013-04-10 16:11:48 +0900

    a trivial refactoring

--- spec/controllers/parameters_controller_spec.rb
+++ spec/controllers/parameters_controller_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 
 describe ParametersController do
 
+  # This should return the minimal set of values that should be in the session
+  # in order to pass any filters (e.g. authentication) defined in
+  # SimulatorsController. Be sure to keep this updated too.
+  def valid_session
+    {}
+  end
+  
   describe "GET 'show'" do
     it "returns http success" do
       sim = FactoryGirl.create(:simulator)
-      get 'show', id: sim.parameters.first, simulator_id: sim
+      get 'show', {id: sim.parameters.first, simulator_id: sim}, valid_session
       response.should be_success
     end
   end
