fe742fdd7414cc2af5da85ae325b23aee87e9aba
Yohsuke Murase <flyingcamelspecial@gmail.com>
2013-04-10 10:58:05 +0900

    add Parameter#show page

--- app/controllers/parameters_controller.rb
+++ app/controllers/parameters_controller.rb
@@ -1,5 +1,12 @@
 class ParametersController < ApplicationController
 
   def show
+    @parameter = Parameter.find(params[:id])
+    @simulator = @parameter.simulator
+    @parameter_keys = @simulator.parameter_keys.keys
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @parameter }
+    end
   end
 end
