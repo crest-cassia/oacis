8f5b28c9507d5bf80995f822ac5737d0fbbbb877
Yohsuke Murase <flyingcamelspecial@gmail.com>
2013-04-10 09:51:49 +0900

    Added parameters table to Simulator#show

---
+++ app/controllers/parameters_controller.rb
@@ -0,0 +1,5 @@
+class ParametersController < ApplicationController
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 AcmProto::Application.routes.draw do
-  resources :simulators, :only => ["index", "show"]
+  resources :simulators, :only => ["index", "show"] do
+    resources :parameters, :only => ["show"]
+  end
 
   authenticated :user do
     root :to => 'home#index'
