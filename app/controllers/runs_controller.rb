78f8824b676e2f13607c5281b3667c5489f768fc
Yohsuke Murase <flyingcamelspecial@gmail.com>
2013-04-11 19:30:25 +0900

    implemented RunsController and its views

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,7 +1,33 @@
 class RunsController < ApplicationController
+
   def show
+    @run = Run.find(params[:id])
+    @parameter = @run.parameter
+    @simulator = @parameter.simulator
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @run }
+    end
   end
 
   def create
+    @parameter = Parameter.find(params[:parameter_id])
+    @run = @parameter.runs.build(params[:run])
+    respond_to do |format|
+      if @run.save
+        format.html {
+          redirect_to simulator_parameter_run_path(@parameter.simulator, @parameter, @run),
+            notice: 'Run was successfully created.'
+        }
+        format.json { render json: @run, status: :created, location: @run}
+      else
+        format.html {
+          redirect_to simulator_parameter_path(@parameter.simulator, @parameter),
+            error: 'Failed to create a run.'
+        }
+        format.json { render json: @run.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
 end
--- spec/controllers/runs_controller_spec.rb
+++ spec/controllers/runs_controller_spec.rb
@@ -2,18 +2,88 @@ require 'spec_helper'
 
 describe RunsController do
 
+  # This should return the minimal set of values that should be in the session
+  # in order to pass any filters (e.g. authentication) defined in
+  # ParametersController. Be sure to keep this updated too.
+  def valid_session
+    {}
+  end
+
+  before(:each) do
+    @sim = FactoryGirl.create(:simulator)
+    @par = @sim.parameters.first
+    @run = @par.runs.first
+  end
+
   describe "GET 'show'" do
+
     it "returns http success" do
-      get 'show'
+      get 'show', {simulator_id: @sim, parameter_id: @par, id: @run}, valid_session
       response.should be_success
+    end
+
+    it "assigns instance variables" do
+      get 'show', {simulator_id: @sim, parameter_id: @par, id: @run}, valid_session
+      assigns(:run).should eq(@run)
+      assigns(:parameter).should eq(@par)
+      assigns(:simulator).should eq(@sim)
     end
   end
 
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
+  describe "POST 'create'" do
+
+    before(:each) do
+      @req_param = {simulator_id: @sim, parameter_id: @par}
+    end
+
+    describe "with valid parameters" do
+
+      it "creates a new run" do
+        expect {
+          post 'create', @req_param, valid_session
+        }.to change(Run.where(parameter_id: @par), :count).by(1)
+      end
+
+      it "redirects to show page" do
+        post 'create', @req_param, valid_session
+        run = Run.last
+        parameter = run.parameter
+        response.should redirect_to(
+          simulator_parameter_run_path(parameter.simulator, parameter, run)
+        )
+      end
+
+      it "assigns seed specified by request parameter" do
+        seed_val = 12345
+        @req_param.update(run: {seed: seed_val})
+        post 'create', @req_param, valid_session
+        Run.where(parameter_id: @par).last.seed.should == seed_val
+      end
+    end
+
+    describe "with invalid parameters" do
+
+      it "raises an error when the Parameter is not found" do
+        @req_param.update(parameter_id: 1234)
+        lambda {
+          post 'create', @req_param, valid_session
+        }.should raise_error
+      end
+
+      it "fails with a duplicated seed" do
+        seed_val = @par.runs.first.seed
+        @req_param.update(run: {seed: seed_val})
+        expect {
+          post 'create', @req_param, valid_session
+        }.to change(Run, :count).by(0)
+      end
+
+      it "redirects to parameter#show path" do
+        seed_val = @par.runs.first.seed
+        @req_param.update(run: {seed: seed_val})
+        post 'create', @req_param, valid_session
+        response.should redirect_to(simulator_parameter_path(@sim, @par))
+      end
     end
   end
-
 end
