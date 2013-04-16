823ae239325725ab186cca34b803465dd1f2e029
Yohsuke Murase <flyingcamelspecial@gmail.com>
2013-04-16 15:48:12 +0900

    refined the views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def distance_to_now_in_words(datetime)
+    if datetime
+      return distance_of_time_in_words_to_now(datetime) + ' ago'
+    else
+      return ''
+    end
+  end
 end
--- app/helpers/runs_helper.rb
+++ app/helpers/runs_helper.rb
@@ -4,4 +4,12 @@ module RunsHelper
     public_dir = Rails.root.join('public')
     file_path.to_s.sub(/^#{public_dir.to_s}/, '')
   end
+
+  def formatted_elapsed_time(elapsed_time)
+    if elapsed_time
+      return sprintf("%.2f", elapsed_time)
+    else
+      return ''
+    end
+  end
 end
