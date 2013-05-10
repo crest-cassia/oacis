cd447b452ed0be2783e337204fa59f426b881afb
take <take@take-Aspire-3830TG.(none)>
2013-05-10 15:39:47 +0900

    add field :query, type: Hash add unit test for ParameterQuery add some basic test for ParameterQuery

--- spec/models/parameter_query_spec.rb
+++ spec/models/parameter_query_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe ParameterQuery do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @query = FactoryGirl.create(:parameter_query)
+  end
+  describe "basic test" do
+    before(:each) do
+      @query.query = { "L" => {:eq => 50 } }
+    end
+    subject { @query }
+    it {should be_a(ParameterQuery)}
+    it {should have_at_least(1).query}
+  end
 end
