3fd21250a0f7cb5454b592899dffc5fd9b3f3005
take <take@take-Aspire-3830TG.(none)>
2013-05-10 19:33:20 +0900

    add gem its add Query class on the top of ParameterQuery developed get_selector add add_constraint(parameter_con) add del_constraint(parameter_com)

--- app/models/parameter_query.rb
+++ app/models/parameter_query.rb
@@ -1,4 +1,24 @@
+class Query
+  include Origin::Queryable
+end
+
 class ParameterQuery
   include Mongoid::Document
   field :query, type: Hash
+  def get_selector
+    #p "in get_selector"
+    h = {"v" => self.query}
+    return h
+  end
+  def add_constraint(parameter_con)
+    p "in add_constraint"
+    p parameter_con
+    #p q = Query.new.gte({"v.L" => 1}).where({"v.T" => 2.0})
+    #p ParameterSet.where({"v.L" => 1}).first
+    #p ParameterSet.where(q.selector).first
+    return get_selector
+  end
+  def del_constraint()
+    return get_selector
+  end
 end
--- spec/models/parameter_query_spec.rb
+++ spec/models/parameter_query_spec.rb
@@ -5,12 +5,27 @@ describe ParameterQuery do
   before(:each) do
     @query = FactoryGirl.create(:parameter_query)
   end
-  describe "basic test" do
+  describe "basic parameterquery" do
     before(:each) do
-      @query.query = { "L" => {:eq => 50 } }
+      @query.query = [{"L"=>1}, {"T"=>2.0}]
+      @query.save
     end
     subject { @query }
     it {should be_a(ParameterQuery)}
-    it {should have_at_least(1).query}
+    it {should have_at_least(1).query }
+    its(:query) {should_not be_empty}
+  end
+  describe "method tests" do
+    before(:each) do
+      @query.query = {"L"=>1, "T"=>2.0}
+      @query.save
+      @sim = FactoryGirl.create(:simulator, 
+        parameter_sets_count: 1, 
+        runs_count: 1)
+      end
+    subject { @query }
+    its(:get_selector) {should == Query.new.where(v: {"L"=>1, "T"=>2.0}).selector}
+    its(:add_constraint, {"S"=>20}) {should == Query.new.where(v: {"L"=>1, "T"=>2.0, "S"=>2.0}).selector}
+    its(:del_constraint) {should == Query.new.where(v: {"L"=>1, "T"=>2.0}).selector}
   end
 end
