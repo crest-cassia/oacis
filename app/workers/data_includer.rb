0d91e742a2cd7b68f11b890bf717d42001e57bfd
Yohsuke Murase <flyingcamelspecial@gmail.com>
2013-05-15 17:22:22 +0900

    cm-41 prototype of DataIncluder
    DataIncluder is added.
    It copies files in the working directory to the run directory.
    It also updates attributes of Run following _run_status.json.
    
    File transfer of files in the remote host is not supported yet.

---
+++ app/workers/data_includer.rb
@@ -0,0 +1,51 @@
+class DataIncluder
+
+  QUEUE_NAME = :data_includer_queue
+  @queue = QUEUE_NAME
+
+  STATUS_JSON_FILENAME = SimulatorRunner::STATUS_JSON_FILENAME
+  FILES_TO_SKIP_COPY = ['_input.json', '_output.json', '_run_status.json']
+
+  def self.perform(run_info)
+    run_id = run_info["run_id"]
+    work_dir = Pathname.new(run_info["work_dir"])
+
+    run = Run.find(run_id)
+    copy_files(work_dir, run.dir)
+
+    json_path = work_dir.join(STATUS_JSON_FILENAME)
+    parsed = JSON.load(File.open(json_path))
+    update_run(run, parsed)
+
+    remove_work_dir(work_dir)
+  end
+
+  def self.copy_files(work_dir, run_dir)
+    FileUtils.mkdir_p(run_dir)
+
+    Dir.chdir(work_dir) {
+      Dir.glob('*').each do |file|
+        next if FILES_TO_SKIP_COPY.include?(file)
+        FileUtils.cp_r(file, run_dir)
+      end
+    }
+  end
+
+  def self.update_run(run, status_hash)
+    run.hostname = status_hash["hostname"]
+    run.cpu_time = status_hash["cpu_time"]
+    run.real_time = status_hash["real_time"]
+    run.started_at = DateTime.parse(status_hash["started_at"])
+    run.finished_at = DateTime.parse(status_hash["finished_at"])
+    run.included_at = DateTime.now
+    run.status = status_hash["status"].to_sym
+    run.save!
+  end
+
+  def self.remove_work_dir(work_dir)
+    FileUtils.rm_r(work_dir)
+  end
+
+  def self.on_failure(exception, run_info)
+  end
+end
