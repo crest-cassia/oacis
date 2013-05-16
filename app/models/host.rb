24706c340398cccc98f96c6314f2d6a141343df1
Yohsuke Murase <flyingcamelspecial@gmail.com>
2013-05-16 12:37:23 +0900

    cm-42 add validations and default values to Host

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,12 +1,18 @@
 class Host
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :name, type: String
   field :hostname, type: String
   field :user, type: String
-  field :port, type: Integer
-  field :ssh_key, type: String
-  field :show_status_command, type: String
-  field :submission_command, type: String
-  field :work_base_dir, type: String
-  field :simulator_base_dir, type: String
+  field :port, type: Integer, default: 22
+  field :ssh_key, type: String, default: '~/.ssh/id_rsa'
+  field :show_status_command, type: String, default: 'ps au'
+  field :submission_command, type: String, default: 'nohup'
+  field :work_base_dir, type: String, default: '~'
+  field :simulator_base_dir, type: String, default: '~'
+
+  validates :name, presence: true, uniqueness: true
+  validates :hostname, presence: true
+  validates :user, presence: true
+  validates :port, numericality: {greater_than_or_equal_to: 1, less_than: 65536}
 end
--- spec/models/host_spec.rb
+++ spec/models/host_spec.rb
@@ -1,5 +1,82 @@
 require 'spec_helper'
 
 describe Host do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+
+    before(:each) do
+      @valid_attr = {
+        name: "nameABC",
+        hostname: "localhost",
+        user: ENV['USER'],
+        port: 22,
+        ssh_key: '~/.ssh/id_rsa',
+        show_status_command: 'ps au',
+        submission_command: 'nohup',
+        work_base_dir: '~/__cm_work__',
+        simulator_base_dir: '~'
+      }
+    end
+
+    it "'name' must be present" do
+      @valid_attr.delete(:name)
+      Host.new(@valid_attr).should_not be_valid
+    end
+
+    it "'name' must be unique" do
+      Host.create!(@valid_attr)
+      Host.new(@valid_attr).should_not be_valid
+    end
+
+    it "'hostname' must be present" do
+      @valid_attr.delete(:hostname)
+      Host.new(@valid_attr).should_not be_valid
+    end
+
+    it "'user' must be present" do
+      @valid_attr.delete(:user)
+      Host.new(@valid_attr).should_not be_valid
+    end
+
+    it "default of 'port' is 22" do
+      @valid_attr.delete(:port)
+      Host.new(@valid_attr).port.should eq(22)
+    end
+
+    it "'port' must be between 1..65535" do
+      @valid_attr.update(port: 'abc')  # => casted to 0
+      Host.new(@valid_attr).should_not be_valid
+    end
+
+    it "default of 'ssh_key' is '~/.ssh/id_rsa'" do
+      @valid_attr.delete(:ssh_key)
+      Host.new(@valid_attr).ssh_key.should eq('~/.ssh/id_rsa')
+    end
+
+    it "default of 'show_status_command is 'ps au'" do
+      @valid_attr.delete(:show_status_command)
+      Host.new(@valid_attr).show_status_command.should eq('ps au')
+    end
+
+    it "default of 'submission_command' is 'nohup'" do
+      @valid_attr.delete(:submission_command)
+      Host.new(@valid_attr).submission_command.should eq('nohup')
+    end
+
+    it "default of 'work_base_dir' is '~'" do
+      @valid_attr.delete(:work_base_dir)
+      Host.new(@valid_attr).work_base_dir.should eq('~')
+    end
+
+    it "default of 'simulator_base_dir' is '~'" do
+      @valid_attr.delete(:simulator_base_dir)
+      Host.new(@valid_attr).simulator_base_dir.should eq('~')
+    end
+
+    it "has timestamp fields" do
+      host = Host.new(@valid_attr)
+      host.should respond_to(:created_at)
+      host.should respond_to(:updated_at)
+    end
+  end
 end
