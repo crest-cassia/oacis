480a1acc4fea9cb9c0e98efc497cd90f53bce689
Yohsuke Murase <flyingcamelspecial@gmail.com>
2013-06-18 16:30:19 +0900

    create directory for ParameterSetGroup

--- app/models/parameter_set_group.rb
+++ app/models/parameter_set_group.rb
@@ -5,4 +5,15 @@ class ParameterSetGroup
   belongs_to :simulator
   has_and_belongs_to_many :parameter_sets
   embeds_many :analysis_runs, as: :analyzable
+
+  after_save :create_dir
+
+  public
+  def dir
+    ResultDirectory.parameter_set_group_path(self)
+  end
+
+  def create_dir
+    FileUtils.mkdir_p(self.dir)
+  end
 end
