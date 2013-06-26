e0d651c25e9f01a5255839291242c23fa3c8843d
Yohsuke Murase <flyingcamelspecial@gmail.com>
2013-06-26 18:49:40 +0900

    cm-59 implemented JobSubmitter

--- app/workers/job_submitter.rb
+++ app/workers/job_submitter.rb
@@ -3,6 +3,16 @@ class JobSubmitter
   QUEUE_NAME = :job_submitter_queue
   @queue = QUEUE_NAME
 
-  def self.perform(run_info)
+  MAX_JOBS_FOR_EACH_NODE = 10
+
+  def self.perform
+    Host.all.each do |host|
+      num = MAX_JOBS_FOR_EACH_NODE - host.submitted_runs.count
+      num = 0 if num < 0
+      runs = host.submittable_runs.limit(num)
+      host.submit(runs)
+    end
+
+    # enqueue a job for 5 minutes later
   end
 end
