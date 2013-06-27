fe60b3352d7ce0e934a0cddd47f96cda745f0376
Yohsuke Murase <flyingcamelspecial@gmail.com>
2013-06-27 17:46:37 +0900

    fixed a regression

--- spec/models/parameter_set_spec.rb
+++ spec/models/parameter_set_spec.rb
@@ -191,10 +191,10 @@ describe ParameterSet do
     it "returns the runs count" do
       sim = FactoryGirl.create(:simulator, parameter_sets_count: 1, runs_count: 10)
       prm = sim.parameter_sets.first
-      prm.runs_status_count["total"] = prm.runs.count
-      prm.runs_status_count["finished"].should == prm.runs.where(status: :finished).count
-      prm.runs_status_count["running"].should == prm.runs.where(status: :running).count
-      prm.runs_status_count["failed"].should == prm.runs.where(status: :failed).count
+      prm.runs_status_count[:total] = prm.runs.count
+      prm.runs_status_count[:finished].should == prm.runs.where(status: :finished).count
+      prm.runs_status_count[:running].should == prm.runs.where(status: :running).count
+      prm.runs_status_count[:failed].should == prm.runs.where(status: :failed).count
     end
   end
 end
