86a9ffef173361e44d2ae215b21d1cb85fe5753c
Yohsuke Murase <flyingcamelspecial@gmail.com>
2013-07-04 13:13:58 +0900

    removed built files from repository

--- doc/build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({objects:{},terms:{index:0,search:0,welcom:0,modul:0,indic:0,manag:0,content:0,tabl:0,cassia:0,document:0,page:0},objtypes:{},titles:["Welcome to CASSIA Manager&#8217;s documentation!"],objnames:{},filenames:["index"]})
