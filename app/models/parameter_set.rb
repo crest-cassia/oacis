6cb93ed9e8ea4572768954e0db28917e928f3d80
Yohsuke Murase <flyingcamelspecial@gmail.com>
2013-07-11 11:08:59 +0900

    use cached runs_count when displaying progress bar

--- app/models/parameter_set.rb
+++ app/models/parameter_set.rb
@@ -2,10 +2,10 @@ class ParameterSet
   include Mongoid::Document
   include Mongoid::Timestamps
   field :v, type: Hash
-  field :total_runs_count, type: Integer, default: 0
-  field :finished_runs_count, type: Integer, default: 0
-  field :running_runs_count, type: Integer, default: 0
-  field :failed_runs_count, type: Integer, default: 0
+  field :total_runs_count, type: Integer
+  field :finished_runs_count, type: Integer
+  field :running_runs_count, type: Integer
+  field :failed_runs_count, type: Integer
   belongs_to :simulator
   has_many :runs
   has_many :analyses, as: :analyzable
@@ -38,11 +38,14 @@ class ParameterSet
   end
 
   def runs_status_count
+    unless total_runs_count and finished_runs_count and running_runs_count and failed_runs_count
+      update_runs_count
+    end
     counts = {}
-    counts[:total] = runs.count
-    counts[:finished] = runs.where(status: :finished).count
-    counts[:running] = runs.where(status: :running).count
-    counts[:failed] = runs.where(status: :failed).count
+    counts[:total] = total_runs_count
+    counts[:finished] = finished_runs_count
+    counts[:running] = running_runs_count
+    counts[:failed] = failed_runs_count
     counts
   end
 
