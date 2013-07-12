f7b4ad5b772cbf8378f80d1eca516f74ab9389d9
Yohsuke Murase <flyingcamelspecial@gmail.com>
2013-07-12 10:43:09 +0900

    removed User model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -49,10 +49,5 @@ AcmProto::Application.routes.draw do
 
   resources :hosts
 
-  authenticated :user do
-    root :to => 'home#index'
-  end
-  root :to => "home#index"
-  devise_for :users
-  resources :users
+  root :to => "simulators#index"
 end
