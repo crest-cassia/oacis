571d0555567417052c28bbb1431edaec376780ed
Yohsuke Murase <flyingcamelspecial@gmail.com>
2013-08-05 14:05:27 +0900

    add specs for AnalyzersController
    - add specs for AnalyzersController#new and #create

--- spec/controllers/analyzers_controller_spec.rb
+++ spec/controllers/analyzers_controller_spec.rb
@@ -28,6 +28,88 @@ describe AnalyzersController do
     end
   end
 
+  describe "GET new" do
+
+    before(:each) do
+      @sim = FactoryGirl.create(:simulator,
+                                parameter_sets_count: 0, runs_count: 0, analyzers_count: 0)
+    end
+
+    it "assigns a new simulator as @simulator" do
+      get :new, {simulator_id: @sim.to_param}, valid_session
+      assigns(:analyzer).should be_a_new(Analyzer)
+    end
+  end
+
+  describe "POST create" do
+
+    before(:each) do
+      @sim = FactoryGirl.create(:simulator,
+                                parameter_sets_count: 0, runs_count: 0, analyzers_count: 0)
+    end
+
+    describe "with valid params" do
+
+      before(:each) do
+        definitions = [
+          {key: "param1", type: "Integer"},
+          {key: "param2", type: "Float"}
+        ]
+        analyzer = {
+          name: "analyzerA", type: "on_run", command: "echo",
+          parameter_definitions_attributes: definitions,
+          auto_run: "no", description: "xxx yyy"
+        }
+        @valid_post_parameter = {simulator_id: @sim.id, analyzer: analyzer}
+      end
+
+      it "creates a new Analyzer" do
+        expect {
+          post :create, @valid_post_parameter, valid_session
+        }.to change(Analyzer, :count).by(1)
+      end
+
+      it "assigns attributes of newly created Analyzer" do
+        post :create, @valid_post_parameter, valid_session
+        azr = Analyzer.last
+        azr.name.should eq "analyzerA"
+        azr.type.should eq :on_run
+        azr.command.should eq "echo"
+        azr.auto_run.should eq :no
+        azr.description.should eq "xxx yyy"
+        azr.parameter_definition_for("param1").type.should eq "Integer"
+        azr.parameter_definition_for("param2").type.should eq "Float"
+      end
+
+      it "assigns a newly created analyzer as @analyzer" do
+        post :create, @valid_post_parameter, valid_session
+        assigns(:analyzer).should be_a(Analyzer)
+        assigns(:analyzer).should be_persisted
+      end
+
+      it "redirects to the created analyzer" do
+        post :create, @valid_post_parameter, valid_session
+        response.should redirect_to(Analyzer.last)
+      end
+    end
+
+    describe "with invalid params" do
+
+      it "assigns a newly created but unsaved simulator as @simulator" do
+        expect {
+          post :create, {simulator_id: @sim.id, analyzer: {}}, valid_session
+          assigns(:analyzer).should be_a_new(Analyzer)
+        }.to_not change(Analyzer, :count)
+      end
+
+      it "re-renders the 'new' template" do
+        # Trigger the behavior that occurs when invalid params are submitted
+        post :create, {simulator_id: @sim.id, analyzer: {}}, valid_session
+        response.should render_template("new")
+      end
+    end
+  end
+
   describe "DELETE 'destroy'" do
 
     before(:each) do
