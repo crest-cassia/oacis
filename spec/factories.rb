c442c56ff18e72067c8ebab84ea164c475d61722
Yohsuke Murase <flyingcamelspecial@gmail.com>
2013-08-07 11:02:29 +0900

    move factory of host to factories.rb
    - remove spec/factories/hosts.rb
      - copy its contents to spec/factories.rb

--- spec/factories.rb
+++ spec/factories.rb
@@ -136,4 +136,16 @@ FactoryGirl.define do
       {"T" => {"gte" => n*2.0}, "L"=> {"lte" => n}}
     end
   end
+
+  factory :host do
+    sequence(:name, 'A') {|n| "Host_#{n}"}
+    sequence(:hostname, 'A') {|n| "hostname.#{n}"}
+    user "login_user"
+  end
+
+  factory :localhost, class: Host do
+    name "localhost"
+    hostname { `hostname`.chomp }
+    user {ENV['USER']}
+  end
 end
