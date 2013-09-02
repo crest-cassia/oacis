7f72edc059e3871076898a59753b7c57eb3c0ab1
Takeshi Uchitane <takeshi.uchitane@riken.jp>
2013-09-02 17:38:56 +0900

    parameter_set_list and run_list is sorted so as latest one is top of the list.

--- app/assets/javascripts/runs.js
+++ app/assets/javascripts/runs.js
@@ -6,6 +6,7 @@ $(function() {
       bProcessing: true,
       bServerSide: true,
       bFilter: false,
+      aaSorting: [[ 8, "desc" ]],
       bDestroy: true,
       sAjaxSource: $('#runs_list').data('source'),
       sDom: "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
