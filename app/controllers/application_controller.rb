867b5cc61cb929bfc258dec92843bf951e215a98
Yohsuke Murase <yohsuke.murase@riken.jp>
2013-10-01 12:40:05 +0900

    add digest authentication
    - edit application_controller.rb when you use

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authenticate
+
+  # CHANGE THE PASSWORD
+  USERS = {"admin" => "pass"}
+
+  private
+  def authenticate
+    authenticate_or_request_with_http_digest do |username|
+      USERS[username]
+    end
+  end
 end
