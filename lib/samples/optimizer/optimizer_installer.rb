a7ef45521986431d1840de6f17da69f27888a954
Takeshi Uchitane <takeshi.uchitane@riken.jp>
2013-10-15 16:51:36 +0900

    update optimizer_installer

--- lib/samples/optimizer/optimizer_installer.rb
+++ lib/samples/optimizer/optimizer_installer.rb
@@ -1,43 +1,131 @@
 require 'pp'
 
-puts "This installer regits an optimizer to CM."
-if Simulator.count == 0
-  puts "There are no simulators in CM."
-  exit(-1)
-end
-puts "commands: reset   (discard all settings and return to first stage.)"
-puts "          exit    (discard settings and exit.)"
-puts ""
+class TermColor
+  class << self
+    # default color
+    def reset     ; c  0 ; end
+    def reset_i   ; ci 0 ; end
 
-$stages=["init","set_name","select_simulator","select_managed_parameters","set_managed_parameters","finish"]
-$stage_counter=0
-$sim_id=nil
-$sim=nil
-$managed_params=[]
-$name=nil
+    # colors
+    def red       ; c  31; end
+    def green     ; c  32; end
+    def blue      ; c  34; end
+    def red_i     ; ci 31; end
+    def green_i   ; ci 32; end
+    def blue_i    ; ci 34; end
 
-def simulator_list
-  puts "install stage: "+$stages[$stage_counter]
-  pp Simulator.each_with_index.map{ |s,i| i.to_s+":"+s.name }.join(",")
-  puts "select num:"
-end
+    # out put
+    def c(num)
+      print "\e[#{num.to_s}m"
+    end
 
-def parameter_list
-  puts "install stage: "+$stages[$stage_counter]
-  pp $sim.parameter_definitions.each_with_index.map{|p,i| i.to_s+":"+p["key"].to_s+","+p["type"].to_s+","+p["default"].to_s+","+p["description"].to_s}
-  puts "select nums:"
-end
-
-def set_opt_parameter(index)
-  puts "install stage: "+$stages[$stage_counter]
-  para = $sim.parameter_definitions[index]
-  pp para["key"]
-  puts "set range:"
-  str = STDIN.gets
-  if ["reset","restart"].include?(str.chomp)
-    $stage_counter -= 1
+    def ci(num)
+      "\e[#{num.to_s}m"
+    end
   end
-  if $stages[$stage_counter] == "set_managed_parameters"
+end
+
+class Selector
+  def initialize(stages, initial_data)
+    @@stages=stages
+    @@stage_counter = 0
+    @@initial_data=initial_data
+    @@data_cash=initial_data
+  end
+
+  def run
+    while @@stage_counter < @@stages.length
+      current_step = @@stages[@@stage_counter]
+      current_step.init(@@data_cash)
+      while str = STDIN.gets
+        break if ["exit","quit","bye"].include?(str.chomp)
+        if str.chomp == ""
+          next
+        end
+        if ["reset","restart"].include?(str.chomp)
+          @@stage_counter = 0
+          @@data_cash = @@initial_data
+          next
+        end
+
+        b = current_step.run(str.chomp)
+        break if b == true
+      end
+
+      @@data_cash=current_step.finalize
+      @@stage_counter+=1
+    end
+  end
+end
+
+class OptimizerSelect
+  def init(target_sim)
+    @@steps=["init","set_name","select_analyzer","select_managed_parameters","set_managed_parameters","finish"]
+    @@sim=target_sim
+    @@name=nil
+    @@anz=nil
+    @@managed_params=[]
+    @@step_counter=0
+    message
+    @@step_counter += 1
+    @@opt_param_counter=0
+  end
+
+  def run(str)
+    case @@steps[@@step_counter]
+      when "set_name"
+        set_name(str)
+      when "select_analyzer"
+        select_analyzer(str)
+      when "select_managed_parameters"
+        select_managed_parameters(str)
+      when "set_managed_parameters"
+        set_opt_parameter(str)
+    end
+    message
+    @@step_counter += 1
+    return true if @@step_counter == (@@steps.length - 1)
+  end
+
+  def finalize
+    return @@managed_params
+  end
+
+  private
+  def set_name(str)
+    @@name=str
+    puts "new optimizer name is "+TermColor.blue_i+"\""+@@name+"\""+TermColor.reset_i
+  end
+  
+  def select_analyzer(str)
+    if (str == "0" or str.to_i > 0) and (@@sim.analyzers.count > str.to_i)
+      @@anz=@@sim.analyzers.to_a[str.to_i]
+    else
+      TermColor.red
+      puts "*****************************************"
+      puts "***ERROR:enter a number(Integer less than "+@@sim.analyzers.count.to_s+")***"
+      puts "*****************************************"
+      TermColor.reset
+      @@step_counter -=1
+    end
+  end
+
+  def select_managed_parameters(str)
+    @@params = str.split(",").map{|s| s.to_i if (s == "0" or s.to_i > 0) and (@@sim.parameter_definitions.count > s.to_i)}.uniq.compact
+    if @@params.length > 0
+      pp @@params.map{|p| p.to_s+":"+@@sim.parameter_definitions[p].key}
+    else
+      TermColor.red
+      puts "*****************************************"
+      puts "***ERROR:enter numbers(Integer less than "+@@sim.parameter_definitions.count.to_s+")***"
+      puts "*****************************************"
+      TermColor.reset
+      @@step_counter -=1
+    end
+  end
+
+  def set_opt_parameter(str)
+    para=@@sim.parameter_definitions[@@params[@@opt_param_counter]]
     case para["type"]
     when "Integer"
       range = str.chomp.split(",").map{|s| s.to_i}
@@ -48,71 +136,153 @@ def set_opt_parameter(index)
     when "String"
       range = str.chomp.split(",")
     end
-  end
-  $managed_params[index]={"key" => para["key"], "type" => para["type"], "default" => para["default"], "descritption" => para["descritoteion"], "range" => range}
-end
-
-def input_name
-  puts "Input optimizer name"
-end
-
-def message(stage)
-  case stage
-  when "init"
-    $stage_counter += 1
-    input_name
-  when "set_name"
-    $stage_counter += 1
-    simulator_list
-  when "select_simulator"
-    $stage_counter += 1
-    parameter_list
-  end
-end
-
-message($stages[$stage_counter])
-
-while str = STDIN.gets
-  break if ["exit","quit","bye"].include?(str.chomp)
-  if str.chomp == ""
-    next
-  end
-  if ["reset","restart"].include?(str.chomp)
-    $stage_counter = 0
-    message($stage_counter)
-    next
+    @@managed_params[@@params[@@opt_param_counter]]={"key" => para["key"], "type" => para["type"], "default" => para["default"], "descritption" => para["descritoteion"], "range" => range}
+    @@opt_param_counter+=1
+    @@step_counter -= 1 unless @@opt_param_counter == @@params.length
   end
 
-  case $stages[$stage_counter]
+  def input_name
+    TermColor.green
+    puts "install stage: "+@@steps[@@step_counter+1]
+    TermColor.reset
+    puts "Input optimizer name:"
+  end
+
+  def analyzer_list
+    TermColor.green
+    puts "install stage: "+@@steps[@@step_counter+1]
+    TermColor.reset
+    pp @@sim.analyzers.each_with_index.map{ |s,i| i.to_s+":"+s.name }.join(",")
+    puts "select num:"
+  end
+
+  def parameter_list
+    puts "selected analyzer is "+TermColor.blue_i+@@anz.name+TermColor.reset_i
+    TermColor.green
+    puts "install stage: "+@@steps[@@step_counter+1]
+    TermColor.reset
+    pp @@sim.parameter_definitions.each_with_index.map{|p,i| i.to_s+":"+p["key"].to_s+","+p["type"].to_s+","+p["default"].to_s+","+p["description"].to_s}
+    puts "select nums:"
+  end
+
+  def managed_parameter_message
+    TermColor.green
+    puts "install stage: "+@@steps[@@step_counter]
+    TermColor.reset
+    para = @@sim.parameter_definitions[@@params[@@opt_param_counter]]
+    pp para["key"]
+    puts "set range:[min, max(, span)]"
+  end
+
+  def show_result
+    puts ""
+    TermColor.green
+    puts "New optimizer is installed."
+    TermColor.reset
+    puts "Optimizer name is "+TermColor.blue_i+"\""+@@name+"\""+TermColor.reset_i
+    puts "Target simulator is "+TermColor.blue_i+"\""+@@sim.name+"\""+TermColor.reset_i
+    puts "Target analyzer is "+TermColor.blue_i+"\""+@@anz.name+"\""+TermColor.reset_i
+    puts "Managed parameter(s) is(are)"
+    TermColor.blue
+    pp @@managed_params.compact
+    TermColor.reset
+  end
+
+  def message
+    case @@steps[@@step_counter]
+    when "init"
+      input_name
     when "set_name"
-      $name=str.chomp
-      message($stages[$stage_counter])
-    when "select_simulator"
-      if str == "0" or str.to_i
-        $sim_id=Simulator.all.to_a[str.to_i].id
-        $sim=Simulator.find($sim_id)
-        message($stages[$stage_counter])
-      end
+      analyzer_list
+    when "select_analyzer"
+      parameter_list
     when "select_managed_parameters"
-      params = str.chomp.split(",").map{|s| s.to_i}
-      pp params
-      $stage_counter += 1
-      params.each do |i|
-        if $stages[$stage_counter] == "set_managed_parameters"
-          set_opt_parameter(i)
-        end
-      end
-      if $stages[$stage_counter] == "set_managed_parameters"
-        $stage_counter += 1
-      else
-        $stage_counter = 0
-      end
+      managed_parameter_message
+    when "set_managed_parameters"
+      show_result
+    end
   end
-  break if $stage_counter == ($stages.length - 1)
 end
 
+class SimulationSelect
+  def init(target_sims)
+    @@steps=["init","select_simulator","finish"]
+    @@target_sims=target_sims
+    @@sim=nil
+    @@step_counter=0
+    message
+    @@step_counter += 1
+  end
+
+  def run(str)
+    case @@steps[@@step_counter]
+    when "select_simulator"
+      select_simulator(str)
+      message
+    end
+    @@step_counter += 1
+    return true if @@step_counter == @@steps.length-1
+  end
+
+  def finalize
+    return @@sim
+  end
+
+  private
+  def select_simulator(str)
+    if (str == "0" or str.to_i > 0) and (@@target_sims.count > str.to_i)
+      @@sim=@@target_sims.to_a[str.to_i]
+    else
+      TermColor.red
+      puts "*****************************************"
+      puts "***ERROR:enter a number(Integer less than "+@@target_sims.count.to_s+")***"
+      puts "*****************************************"
+      TermColor.reset
+      @@step_counter -=1
+    end
+  end
+
+  def simulator_list
+    TermColor.green
+    puts "install stage: "+@@steps[@@step_counter+1]
+    TermColor.reset
+    pp @@target_sims.each_with_index.map{ |s,i| i.to_s+":"+s.name }.join(",")
+    puts "select num:"
+  end
+
+  def show_result
+    puts "selected simulator is "+TermColor.blue_i+"\""+@@sim.name+"\""+TermColor.reset_i
+  end
+
+  def message
+    case @@steps[@@step_counter]
+    when "init"
+      simulator_list
+    when "select_simulator"
+      show_result
+    end
+  end
+end
+
+def target_simulators
+  if $target_simulators==nil
+    $target_simulators=Simulator.all.select {|sim| sim.analyzers.where(type: :on_run).count > 0}
+  end
+  $target_simulators
+end
+
+TermColor.green
+puts "This installer regits an optimizer to CM."
+
+if target_simulators.count == 0
+  puts "There are no simulators to optimize in CM."
+  exit(-1)
+end
+
+puts "commands: reset   (discard all settings and return to first stage.)"
+puts "          exit    (discard settings and exit.)"
 puts ""
-puts "New optimizer is installed."
-puts "Optimizer name is \""+$name+"\""
-puts "Managed parameter(s) is(are)"
-pp $managed_params
+TermColor.reset
+
+selector = Selector.new([SimulationSelect.new,OptimizerSelect.new],target_simulators)
+selector.run
