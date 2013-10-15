8616947bb5e32a3da193c208345f0578efa993c1
Yohsuke Murase <yohsuke.murase@riken.jp>
2013-10-15 14:22:51 +0900

    trap TERM for clean stop

--- app/workers/worker.rb
+++ app/workers/worker.rb
@@ -7,12 +7,24 @@ class Worker < DaemonSpawn::Base
     @logger.level = Logger::INFO
     @logger.info("starting")
 
+    @term_received = false
+    trap('TERM') {
+      @term_received = true
+      @logger.info("TERM received. stopping")
+    }
+
     loop do
       JobSubmitter.perform(@logger)
+      break if @term_received
       JobObserver.perform(@logger)
+      break if @term_received
       AnalyzerRunner.perform(@logger)
+      break if @term_received
       sleep INTERVAL
+      break if @term_received
     end
+
+    @logger.info("stopped")
   end
 
   def stop
@@ -24,7 +36,7 @@ if $0 == __FILE__
   Worker.spawn!(log_file: Rails.root.join('log', "worker_#{Rails.env}.log"),
                 pid_file: Rails.root.join('tmp', 'pids', "worker_#{Rails.env}.pid"),
                 sync_log: true,
-                working_dir: Rails.root
+                working_dir: Rails.root,
                 singleton: true
                 )
 end
