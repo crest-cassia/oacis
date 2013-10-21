3797bbbbbf27c27ee5e58712555a04dca78bf9a8
Takeshi Uchitane <takeshi.uchitane@riken.jp>
2013-10-21 12:44:08 +0900

    a bug fixed in host#check_submitted_job_status

--- app/workers/job_observer.rb
+++ app/workers/job_observer.rb
@@ -4,7 +4,7 @@ class JobObserver
     # if @@last_updated_at and @last_updated_at - DateTime.now
     Host.all.each do |host|
       logger.info("observing jobs on #{host.name}")
-      host.check_submitted_job_status
+      host.check_submitted_job_status(logger)
     end
   rescue => ex
     logger.error("Error in JobObserver: #{ex.inspect}")
