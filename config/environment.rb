477ac45614751e6ee7458ee1c971284012944381
Yohsuke Murase <yohsuke.murase@riken.jp>
2013-10-23 17:00:21 +0900

    display APP version in the footer

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+APP_VERSION = `git describe --always` unless defined? APP_VERSION
+
 # Initialize the rails application
 AcmProto::Application.initialize!
