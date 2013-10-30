6ac91848da9da5e5820877d2b31f625b270c6d89
Yohsuke Murase <yohsuke.murase@riken.jp>
2013-10-30 17:06:00 +0900

    fix the path of the sample simulator and analyzer

--- lib/samples/module/optimizer/problem01/sample_problem_installer.rb
+++ lib/samples/module/optimizer/problem01/sample_problem_installer.rb
@@ -19,7 +19,7 @@ end
 sim = Simulator.new
 sim.name = "ToyProblem01"
 sim.parameter_definitions = parameter_definitions
-sim.command = Rails.root.to_s+"/lib/samples/problem01/toy_problem01.sh"
+sim.command = File.expand_path( File.join( File.dirname(__FILE__), "toy_problem01.sh") )
 sim.support_input_json = false
 sim.support_mpi = false
 sim.support_omp = false
@@ -35,7 +35,8 @@ anz.simulator_id=sim.to_param
 anz.auto_run = :yes
 anz.type = :on_run
 anz.name = "Sample_Analyzer_for_toy_problem01"
-anz.command = "ruby "+Rails.root.to_s+"/lib/samples/problem01/toy_problem01_analyzer.rb"
+analyzer_path = File.expand_path( File.join(File.dirname(__FILE__), "toy_problem01_analyzer.rb") )
+anz.command = "ruby #{analyzer_path}"
 anz.description = "get Fitness from a result on run."
 anz.save!
 host = Host.where({name: "localhost"}).first
