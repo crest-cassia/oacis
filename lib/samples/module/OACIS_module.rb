6fddc41f6ff2d728397f882e86e3c96b81ca2afe
Takeshi Uchitane <takeshi.uchitane@riken.jp>
2013-11-01 11:16:46 +0900

    refactoring

--- lib/samples/module/OACIS_module.rb
+++ lib/samples/module/OACIS_module.rb
@@ -12,8 +12,7 @@ class OacisModule
     @num_iterations = 0
     loop do
       $stdout.puts "Iteration: #{@num_iterations}"
-      generated_runs = generate_runs
-      wait_runs( generated_runs )
+      wait_runs( generate_runs )
       evaluate_runs
       @num_iterations += 1
       dump_serialized_data
@@ -27,16 +26,11 @@ class OacisModule
 
   def wait_runs( generated_runs )
     loop do
-      runs_finished = generated_runs.all? {|run|
+      runs_finished = generated_runs.all? do |run|
         run.reload unless run.status == :finished or run.status == :failed
-        if run.status == :failed
-          raise "Run #{run} failed"
-        elsif run.status == :finished
-          all_analyzer_finished( run )
-        else
-          false
-        end
-      }
+        raise "Run #{run} failed" if run.status == :failed
+        run.status == :finished and all_analyzer_finished( run )
+      end
       break if runs_finished
       sleep WAIT_INTERVAL
     end
@@ -46,9 +40,7 @@ class OacisModule
     auto_run_analyzers = run.simulator.analyzers.where(type: :on_run, auto_run: :yes)
     auto_run_analyzers.all? do |azr|
       anl = run.analyses.where(analyzer: azr).last
-      if anl and anl.status == :failed
-        raise "Analysis #{anl} failed" if anl.status == :failed
-      end
+      raise "Analysis #{anl} failed" if anl and anl.status == :failed
       anl and anl.status == :finished
     end
   end
