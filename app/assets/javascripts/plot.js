1070784b309bace679e75a197cb942af01e16913
Yohsuke Murase <yohsuke.murase@riken.jp>
2013-12-09 14:19:30 +0900

    initial implementation of drawing scatter plot
    - [TODO] refactoring is necessary

--- app/assets/javascripts/plot.js
+++ app/assets/javascripts/plot.js
@@ -206,5 +206,114 @@ function draw_line_plot(url, parameter_set_base_url) {
 }
 
 function draw_scatter_plot(url, parameter_set_base_url) {
-  console.log("draw_scatter_plot");
+  var margin = {top: 10, right: 100, bottom: 100, left: 100};
+  var width = 560;
+  var height = 460;
+
+  var row = d3.select("#plot").insert("div","div").attr("class", "row");
+  var plot_region = row.append("div").attr("class", "span8");
+  var description = row.append("div").attr("class", "span4");
+
+  var svg = plot_region.insert("svg")
+    .attr({
+      "width": width + margin.left + margin.right,
+      "height": height + margin.top + margin.bottom
+    })
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var progress = show_loading_spin_arc(svg, width, height);
+
+  d3.json(url, function(dat) {
+    progress.remove();
+
+    var xScale = d3.scale.linear().range([0, width]);
+    var yScale = d3.scale.linear().range([height, 0]);
+    var colorScale = d3.scale.linear().range(["blue", "red"])
+
+    xScale.domain([
+      d3.min( dat.data, function(d) { return d[0];}),
+      d3.max( dat.data, function(d) { return d[0];})
+    ]).nice();
+    yScale.domain([
+      d3.min( dat.data, function(d) { return d[1];}),
+      d3.max( dat.data, function(d) { return d[1];})
+    ]).nice();
+    colorScale.domain([
+      d3.min( dat.data, function(d) { return d[2];}),
+      d3.max( dat.data, function(d) { return d[2];})
+    ]).nice();
+
+    console.log(xScale.domain);
+
+    // X-Axis
+    var xAxis = d3.svg.axis()
+      .scale(xScale)
+      .orient("bottom");
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+      .append("text")
+        .style("text-anchor", "middle")
+        .attr("x", width / 2.0)
+        .attr("y", 50.0)
+        .text(dat.xlabel);
+
+    // Y-Axis
+    var yAxis = d3.svg.axis()
+      .scale(yScale)
+      .orient("left");
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height/2)
+        .attr("y", -50.0)
+        .style("text-anchor", "middle")
+        .text(dat.ylabel);
+
+    // draw circles
+    var tooltip = d3.select("#plot-tooltip");
+    var mapped = dat.data.map(function(v) {
+      return {
+        x: v[0], y: v[1],
+        average: v[2], error: v[3], psid: v[4]
+      };
+    });
+    console.log(mapped);
+    var point = svg.selectAll("circle")
+      .data(mapped).enter();
+    point.append("circle")
+      .attr("cx", function(d) { return xScale(d.x);})
+      .attr("cy", function(d) { return yScale(d.y);})
+      .style("fill", function(d) { return colorScale(d.average);})
+      .attr("r", 3)
+      .on("mouseover", function(d) {
+        tooltip.transition()
+          .duration(200)
+          .style("opacity", .8);
+        tooltip.html(
+          dat.xlabel + " : " + d.x + "<br/>" +
+          dat.ylabel + " : " + d.y + "<br/>" +
+          "Result : " + Math.round(d.average*1000000)/1000000 +
+          " (" + Math.round(d.error*1000000)/1000000 + ")<br/>" +
+          "ID: " + d.psid);
+      })
+      .on("mousemove", function() {
+        tooltip
+          .style("top", (d3.event.pageY-10) + "px")
+          .style("left", (d3.event.pageX+10) + "px");
+      })
+      .on("mouseout", function() {
+        tooltip.transition()
+          .duration(300)
+          .style("opacity", 0);
+      })
+      .on("dblclick", function(d) {
+        // open a link in a background window
+        window.open(parameter_set_base_url + d.psid, '_blank');
+      });
+  });
 }
