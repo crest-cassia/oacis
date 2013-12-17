7437e9367b6ff1fc7a9799652704cfe4bbb80859
Yohsuke Murase <yohsuke.murase@riken.jp>
2013-12-17 22:26:11 +0900

    updated usage

--- lib/cli/oacis_cli_common.rb
+++ lib/cli/oacis_cli_common.rb
@@ -6,21 +6,21 @@ class OacisCli < Thor
   USAGE = <<"EOS"
 usage:
 #1 make host.json file
-  ruby oacis_cli.rb show_host -o host.json
+  oacis_cli.rb show_host -o host.json
   #check or edit host.json
 #2 create simulator
-  ruby oacis_cli.rb create_simulator -h host.json -i simulator.json -o simulator_id.json
-  #you can get simulator.json template file "ruby oacis_cli.rb simulator_template -o simulator.json"
+  oacis_cli create_simulator -h host.json -i simulator.json -o simulator_id.json
+  #you can get simulator.json template file "oacis_cli simulator_template -o simulator.json"
   #edit simulator.json (at least following fields, "name", "command", "parameter_definitions")
 #3 create parameter_set
-  ruby oacis_cli.rb create_parameter_sets -s simulator_id.json -i parameter_sets.json -o parameter_set_ids.json
-  #you can get parameter_sets.json template file "ruby oacis_cli.rb parameter_sets_template -s simulator_id.json -o parameter_sets.json"
-#4 create run template
-  ruby oacis_cli.rb runs_template -p parameter_set_ids.json -h host.json -t 1 -o runs.json
-#5 create run
-  ruby oacis_cli.rb create_runs -p parameter_set_ids.json -h host.json -i runs.json -o run_ids.json
+  oacis_cli create_parameter_sets -s simulator_id.json -i parameter_sets.json -o parameter_set_ids.json
+  #you can get parameter_sets.json template file "oacis_cli parameter_sets_template -s simulator_id.json -o parameter_sets.json"
+#4 create job parameter template
+  oacis_cli job_parameter_template -h host_id -o job_parameters.json
+#5 edit job_parameters.json. Then create run
+  oacis_cli create_runs -p parameter_set_ids.json -j job_parameters.json -n 1 -o run_ids.json
 #6 check run status
-  ruby oacis_cli.rb run_status -r run_ids.json
+  oacis_cli run_status -r run_ids.json
 EOS
 
   desc 'usage', "print usage"
