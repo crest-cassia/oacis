e027eeae3aa2b6c1dc162fb8d2fcaf2224a81621
Yohsuke Murase <yohsuke.murase@riken.jp>
2014-01-17 16:39:22 +0900

    rename to JobObserver

--- app/workers/job_observer.rb
+++ app/workers/job_observer.rb
@@ -1,12 +1,15 @@
-class JobMonitoringWorker
+class JobObserver
 
   def self.perform(logger)
     Host.all.each do |host|
       begin
         host.check_submitted_job_status(logger)
       rescue => ex
-        logger.error("Error in JobMonitoringWorker: #{ex.inspect}")
+        logger.error("Error in JobObserver: #{ex.inspect}")
       end
     end
+  end
+
+  def self.observe_host
   end
 end
--- app/workers/worker.rb
+++ app/workers/worker.rb
@@ -19,7 +19,7 @@ class Worker < DaemonSpawn::Base
     loop do
       JobSubmitter.perform(@logger)
       break if @term_received
-      JobMonitoringWorker.perform(@logger)
+      JobObserver.perform(@logger)
       break if @term_received
       AnalyzerRunner.perform(@logger)
       break if @term_received
