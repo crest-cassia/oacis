8083fee852318625ab5b8d467e3aa3e958f58334
Takeshi Uchitane <takeshi.uchitane@riken.jp>
2014-03-04 15:33:32 +0900

    add ga optimizer

---
+++ lib/samples/module/optimizer/ga/ga_runner.rb
@@ -0,0 +1,22 @@
+require 'json'
+
+require_relative '/home/t-uchitane/git/cassia/lib/samples/module/optimizer/ga/ga.rb'
+
+def load_input_data
+  if File.exist?("_input.json")
+    io = File.open('_input.json', 'r')
+    parsed = JSON.load(io)
+    return parsed
+  end
+end
+
+input_data = load_input_data
+
+if input_data.blank?
+  raise "_input.json is missing."
+end
+
+input_data["_target"]=JSON.parse(input_data["_target"])
+input_data["_managed_parameters"]=JSON.parse(input_data["_managed_parameters"])
+
+Ga.new(input_data).run
