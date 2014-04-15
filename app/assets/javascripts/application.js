5c874339b386dfd511ced4211a6aa2e06a709359
Yohsuke Murase <yohsuke.murase@riken.jp>
2014-04-15 15:31:31 +0900

    load only sortable module of jQueryUI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
-//= require jquery.ui.all
+//= require jquery.ui.sortable
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
