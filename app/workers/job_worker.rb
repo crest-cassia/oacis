ca7d54de02acef5afdbe0a2cc21064b9763772eb
Yohsuke Murase <yohsuke.murase@riken.jp>
2014-05-29 15:24:54 +0900

    trivial refactoring

--- app/workers/job_worker.rb
+++ app/workers/job_worker.rb
@@ -54,10 +54,10 @@ end
 
 if $0 == __FILE__
   JobWorker.spawn!(log_file: JobWorker::WORKER_LOG_FILE,
-                  pid_file: JobWorker::WORKER_PID_FILE,
-                  sync_log: true,
-                  working_dir: Rails.root,
-                  singleton: true,
-                  timeout: 30
-                  )
+                   pid_file: JobWorker::WORKER_PID_FILE,
+                   sync_log: true,
+                   working_dir: Rails.root,
+                   singleton: true,
+                   timeout: 30
+                   )
 end
--- app/workers/service_worker.rb
+++ app/workers/service_worker.rb
@@ -52,7 +52,7 @@ end
 
 if $0 == __FILE__
   ServiceWorker.spawn!(log_file: ServiceWorker::WORKER_LOG_FILE,
-                      pid_file: ServiceWorker::WORKER_PID_FILE,
+                       pid_file: ServiceWorker::WORKER_PID_FILE,
                        sync_log: true,
                        working_dir: Rails.root,
                        singleton: true,
