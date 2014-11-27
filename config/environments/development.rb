1b544b06a5e750904f164f1097d9fcb8a8afd5c4
Yohsuke Murase <yohsuke.murase@gmail.com>
2014-11-27 14:54:47 +0900

    buf fix of log path
    - when cli is executed from the directory which is not the project root,
      the log path was not correctly specified

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,7 @@ AcmProto::Application.configure do
   config.assets.debug = true
 
   config.log_level = :error
-  config.logger = Logger.new("log/development.log", 5, 1.megabytes)
+  config.logger = Logger.new(Rails.root.join("log/development.log"), 5, 1.megabytes)
   Mongoid.logger.level = Logger::WARN
   Moped.logger.level = Logger::WARN
 end
