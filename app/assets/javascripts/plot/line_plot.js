e55d7dcefbcfa2ea4438e97c736d705eac6646fb
Yohsuke Murase <yohsuke.murase@gmail.com>
2014-12-11 14:17:59 +0900

    show URL of the line_plot

--- app/assets/javascripts/plot/line_plot.js
+++ app/assets/javascripts/plot/line_plot.js
@@ -283,6 +283,27 @@ LinePlot.prototype.AddDescription = function() {
       dl.append("dt").text("Series");
       dl.append("dd").text(plot.data.series);
     }
+    if(plot.data.irrelevants.length > 0) {
+      dl.append("dt").text("Irrevant keys");
+      dl.append("dd").text( plot.data.irrelevants.join(',') );
+    }
+    dl.append("dt").text("URL");
+    dl.append("input")
+      .attr({"type": "text", "readonly": "readonly"})
+      .attr('value', function() {
+        var url = plot.parameter_set_base_url + plot.current_ps_id;
+        var query = ["plot_type=line",
+                     "x_axis=" + encodeURIComponent(plot.data.xlabel),
+                     "y_axis=" + encodeURIComponent(plot.data.ylabel)
+                     ];
+        if( plot.data.series ) {
+          query.push( "series=" + encodeURIComponent(plot.data.series) );
+        }
+        if( plot.data.irrelevants.length > 0 ) {
+          query.push( "irrelevants=" + encodeURIComponent(plot.data.irrelevants.join(',')) );
+        }
+        return location.protocol + '//' + location.host + url + '?' + query.join('&') + '#!tab-plot';
+      });
   }
   add_label_table();
 
