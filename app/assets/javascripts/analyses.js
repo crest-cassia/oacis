8f39074044a5f382840592431b905a2ccece697e
Yohsuke Murase <yohsuke.murase@gmail.com>
2014-12-11 10:05:57 +0900

    refactoring

--- app/assets/javascripts/analyses.js
+++ app/assets/javascripts/analyses.js
@@ -3,8 +3,8 @@ $(function () {
     if( $(this).attr("state") === "close" ) {
       var tr_element = $(this).closest("tr");
       var analysis_id = $(this).attr("analysis_id");
-      var table_cols = tr_element.children("td").length
-      $(this).attr("state", "open").attr("src", "/assets/collapse.png")
+      var table_cols = tr_element.children("td").length;
+      $(this).attr("state", "open").attr("src", "/assets/collapse.png");
       $.get("/analyses/" + analysis_id + "/_result", {}, function(data) {
         tr_element.after(
           $("<tr>").attr("id", "result_" + analysis_id).html(
@@ -16,9 +16,9 @@ $(function () {
       });
     }
     else {  // state === "open"
-      $(this).attr("state", "close").attr("src", "/assets/expand.png")
-      var result_id = "result_" + $(this).attr("analysis_id")
-      $(this).closest("tr").siblings("tr#" + result_id).remove()
+      $(this).attr("state", "close").attr("src", "/assets/expand.png");
+      var result_id = "result_" + $(this).attr("analysis_id");
+      $(this).closest("tr").siblings("tr#" + result_id).remove();
     }
   });
 });
