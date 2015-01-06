ed26e053b9d83e87a090aa6667ce4b37532ecd2b
Yohsuke Murase <yohsuke.murase@gmail.com>
2015-01-06 17:46:34 +0900

    changed the name of the key
    - disable_auto_reload_tables to auto_reload_tables

--- config/initializers/auto_reload_tables.rb
+++ config/initializers/auto_reload_tables.rb
@@ -1,1 +1,5 @@
-AUTO_RELOAD_TABLES = !(AcmProto::Application.config.user_config["disable_auto_reload_tables"])
+if AcmProto::Application.config.user_config.has_key?("auto_reload_tables")
+  AUTO_RELOAD_TABLES = AcmProto::Application.config.user_config["auto_reload_tables"]
+else
+  AUTO_RELOAD_TABLES = true
+end
