0c1d6ad4257eb046f54b1fc96220c757a2c0340f
Yohsuke Murase <yohsuke.murase@gmail.com>
2015-04-15 16:32:54 +0900

    fixed the status label for failed runs

--- app/helpers/runs_helper.rb
+++ app/helpers/runs_helper.rb
@@ -16,13 +16,13 @@ module RunsHelper
   def status_label(status)
     case status
     when :created
-      '<span class="label">created</span>'
+      '<span class="label label-default">created</span>'
     when :submitted
       '<span class="label label-info">submitted</span>'
     when :running
       '<span class="label label-warning">running</span>'
     when :failed
-      '<span class="label label-important">failed</span>'
+      '<span class="label label-danger">failed</span>'
     when :finished
       '<span class="label label-success">finished</span>'
     else
