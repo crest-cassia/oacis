70d2b6db158e21c683de2892a9bdd13e042f2c6d
takeshi-uchitane <t.uchitane@gmail.com>
2015-04-30 10:46:20 +0900

    before_filter is renamed in before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :authenticate
+  before_action :authenticate
 
   USERS = AcmProto::Application.config.user_config["authentication"]
 
