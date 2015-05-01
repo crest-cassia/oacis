331b8af7e80c04dc0fe4955461d4061cb38be5bd
takeshi-uchitane <t.uchitane@gmail.com>
2015-05-01 18:41:37 +0900

    add hash to url when tabs are selected

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,14 +34,12 @@
 // - Browser scroll position was lost due to fragment navigation
 //   (Worked around this by converting #id values to #!id values before navigating.)
 $(document).ready(function () {
-    if (location.hash.substr(0,2) == "#!") {
-        $("a[href='#" + location.hash.substr(2) + "']").tab("show");
+    if (document.location.hash.substr(0,2) == "#!") {
+      $('.nav-tabs a[href=' + document.location.hash.replace('!', '') + ']').tab('show');
     }
-    $("a[data-toggle='tab']").on("shown", function (e) {
-        var hash = $(e.target).attr("href");
-        if (hash.substr(0,1) == "#") {
-            location.replace("#!" + hash.substr(1));
-        }
+
+    $('.nav-tabs a').on('shown.bs.tab', function(e) {
+      window.location.hash = e.target.hash.replace('#', '#' + '!');
     });
 });
 
