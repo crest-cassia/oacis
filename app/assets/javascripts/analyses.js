5a6ff30a5b97779587f29b7cd777cfd6d38d6a08
takeshi-uchitane <t.uchitane@gmail.com>
2015-05-27 17:06:45 +0900

    analyses table is shown in modal

--- app/assets/javascripts/analyses.js
+++ app/assets/javascripts/analyses.js
@@ -1,25 +1,21 @@
 $(function () {
-  $('body').on("click", 'img[analysis_id]', function() {
-    if( $(this).attr("state") === "close" ) {
-      var tr_element = $(this).closest("tr");
-      var analysis_id = $(this).attr("analysis_id");
-      var table_cols = tr_element.children("td").length;
-      $(this).attr("state", "open").attr("src", "/assets/collapse.png");
-      $.get("/analyses/" + analysis_id + "/_result", {}, function(data) {
-        tr_element.after(
-          $("<tr>").attr("id", "result_" + analysis_id).html(
-            $("<td>").attr({colspan: table_cols}).html(
-              $("<div>").attr("class", "well").html(data)
-            )
-          )
-        );
-      });
-    }
-    else {  // state === "open"
-      $(this).attr("state", "close").attr("src", "/assets/expand.png");
-      var result_id = "result_" + $(this).attr("analysis_id");
-      $(this).closest("tr").siblings("tr#" + result_id).remove();
-    }
+  $("#analyses_list").on("click", "i.fa.fa-plus-square-o[analysis_id]", function() {
+    var analysis_id = $(this).attr("analysis_id");
+    $('#analyses_list_modal').modal("show", {
+      analysis_id: analysis_id
+    });
+  });
+  $("#analyses_list_modal").on('show.bs.modal', function (event) {
+    var analysis_id = event.relatedTarget.analysis_id;
+    $.get("/analyses/"+analysis_id+"/_result", function(data) {
+      $("i.fa.fa-plus-square-o[analysis_id="+analysis_id+"]").attr("state", "open");
+      $("#analyses_list_modal_page").append(data);
+    });
+  });
+
+  $("#analyses_list_modal").on('hidden.bs.modal', function (event) {
+    $('#analyses_list_modal_page').empty();
+    $("i.fa.fa-plus-square-o[analysis_id]").attr("state", "close");
   });
 });
 
@@ -32,8 +28,8 @@ $(function() {
   toggle_auto_reload_analyses_table();
   var oAnalysesTableToReload = null;
   setInterval( function() {
-    var num_open = $('#analyses_list img.treebtn[state="open"]').length;
-    if( bReloadAnalysesTable && num_open == 0 && oAnalysesTableToReload ) { oAnalysesTableToReload.ajax.reload(null, false); }
+    var num_open = $('#analyses_list i.fa.fa-plus-square-o[state="open"]').length;
+    if( bReloadAnalysesTable && num_open === 0 && oAnalysesTableToReload ) { oAnalysesTableToReload.ajax.reload(null, false); }
   }, 5000);
 
   var datatables_for_analyses_table = function() {
