4faa984663d6ef7a5beadffe5197bc1af12a43bf
Yohsuke Murase <yohsuke.murase@gmail.com>
2015-06-19 17:12:27 +0900

    remove deprecation warning
    fixed the following deprecation warning
    Requiring `rspec/autorun` when running RSpec via the `rspec` command is deprecated.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,7 +28,6 @@ Spork.prefork do
   end
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
-  require 'rspec/autorun'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
