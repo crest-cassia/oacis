67eeccadcd78c92f33c55c1c80173ed42da8d254
Yohsuke Murase <yohsuke.murase@gmail.com>
2015-06-20 18:39:01 +0900

    prepare production env
    The following configs are changed from the default production.rb
    - use static files. This is because we don't use Nginx or Apache
    - fallback by asset pipeline when assets are not precompiled
    - set log level to error
    - copy the initialize setting inserted into development.rb
    
    config/mongoid.yml, config/result_directory.yml are also updated
    - production env uses the same resource as the one for development env
    
    To boot OACIS in production env, run the following command
    `bundle exec rake daemon:start RAILS_ENV=production`

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,17 +5,17 @@ AcmProto::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -31,7 +31,7 @@ AcmProto::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :error
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
@@ -46,7 +46,7 @@ AcmProto::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += ["plot/*.js", "explor/*.js"]
+  # config.assets.precompile += ["plot/*.js", "explor/*.js"]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -81,4 +81,8 @@ AcmProto::Application.configure do
 
   config.eager_load = true
 
+  FileUtils.mkdir_p( Rails.root.join("log") )
+  config.logger = Logger.new(Rails.root.join("log/development.log"), 5, 1.megabytes)
+  Mongoid.logger.level = Logger::WARN
+  Moped.logger.level = Logger::WARN
 end
