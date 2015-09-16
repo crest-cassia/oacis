bcb61679dc175f7ed5ff7124ad9d84e0338c6e99
Yohsuke Murase <yohsuke.murase@gmail.com>
2015-09-16 23:26:42 +0900

    refactoring by introducing Worker class

--- app/workers/service_worker.rb
+++ app/workers/service_worker.rb
@@ -1,4 +1,4 @@
-class ServiceWorker < DaemonSpawn::Base
+class ServiceWorker < Worker
 
   INTERVAL = 5
 
@@ -6,49 +6,8 @@ class ServiceWorker < DaemonSpawn::Base
   WORKER_LOG_FILE = Rails.root.join('log', "service_worker_#{Rails.env}.log")
   WORKER_STDOUT_FILE = Rails.root.join('log', "service_worker_#{Rails.env}_out.log")
 
-  def start(args)
-    @logger = Logger.new(WORKER_LOG_FILE, 7)
-    @logger.formatter = LoggerFormatWithTime.new
-    @logger.level = Logger::INFO
-    @logger.info("starting")
-
-    @term_received = false
-    trap('TERM') {
-      @term_received = true
-      @logger.info("TERM received. stopping")
-    }
-
-    loop do
-      CacheUpdater.perform(@logger)
-      break if @term_received
-      sleep INTERVAL
-      break if @term_received
-    end
-
-    @logger.info("stopped")
-  end
-
-  def stop
-    # Never called because trap('TERM') is overwritten
-  end
-
-  def self.alive?
-    if File.file?(WORKER_PID_FILE)
-      pid = (IO.read(WORKER_PID_FILE).to_i)
-      DaemonSpawn.alive? pid
-    else
-      false
-    end
-  end
-
-  # return true if the time stamp of the log file is updated within five minutes
-  LOG_UPDATE_THRESHOLD = 60 * 5 # 5 minutes
-  def self.log_recently_updated?
-    if File.file?(WORKER_LOG_FILE)
-      s = File.stat(WORKER_LOG_FILE)
-      return true if Time.now - s.mtime < LOG_UPDATE_THRESHOLD
-    end
-    false
-  end
+  TASKS = [
+    lambda {|logger| CacheUpdater.perform(@logger) }
+  ]
 end
 
