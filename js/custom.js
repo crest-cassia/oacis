7fe5c123b1faa477047a346bec50f8fb81075988
Yohsuke Murase <yohsuke.murase@gmail.com>
2016-02-21 14:18:23 +0900

    add js to make TOC work well

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,5 @@
 $("table:not([class~='table'])").addClass("table table-striped");
+
+var shiftWindow = function() { scrollBy(0, -70) };
+if (location.hash) shiftWindow();
+window.addEventListener("hashchange", shiftWindow);
