307e80169382a9b22c8ecbffdefc3cc41168e327
Yohsuke Murase <yohsuke.murase@gmail.com>
2016-09-28 13:19:52 +0900

    upgrade jquery-ui-rails, jquery-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
-//= require jquery.ui.sortable
+//= require jquery-ui/sortable
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require analyses.js
