d712251f50a5ec8e7ec67d3188ead76fd5d66aa9
Yohsuke Murase <yohsuke.murase@gmail.com>
2017-01-05 15:16:21 +0900

    unittest for oacis_watcher_py

--- oacis/oacis_watcher.py
+++ oacis/oacis_watcher.py
@@ -4,33 +4,40 @@ class OacisWatcher():
 
     def __init__(self, polling = 5):
         self.polling = polling
-        self.observed_parameter_sets = {}
-        self.observed_parameter_sets_all = {}
+        self._observed_parameter_sets = {}
+        self._observed_parameter_sets_all = {}
 
     def watch_ps(self, ps, callback):
         psid = ps.id().to_s()
-        if psid in self.observed_parameter_sets:
-            self.observed_parameter_sets[psid].append( callback )
+        if psid in self._observed_parameter_sets:
+            self._observed_parameter_sets[psid].append( callback )
         else:
-            self.observed_parameter_sets[psid] = [ callback ]
+            self._observed_parameter_sets[psid] = [ callback ]
+
+    def watch_all_ps(self, ps_array, callback):
+        sorted_ps_ids = tuple( sorted( [ ps.id().to_s() for ps in ps_array ] ) )
+        if sorted_ps_ids in self._observed_parameter_sets_all:
+            self._observed_parameter_sets_all[sorted_ps_ids].append( callback )
+        else:
+            self._observed_parameter_sets_all[sorted_ps_ids] = [ callback ]
 
     def loop(self):
         print("start polling")
         while True:
             executed = True
             while executed:
-                executed = self.check_completed_ps()
-            if len(self.observed_parameter_sets) == 0:
+                executed = (self._check_completed_ps() or self._check_completed_ps_all())
+            if len(self._observed_parameter_sets) == 0 and len(self._observed_parameter_sets_all) == 0:
                 break
             print("waiting for %d sec" % self.polling)
             time.sleep( self.polling )
         print("stop polling")
 
-    def check_completed_ps(self):
+    def _check_completed_ps(self):
         from . import ParameterSet
         executed = False
-        watched_ps_ids = list( self.observed_parameter_sets.keys() )
-        psids = self.completed_ps_ids( watched_ps_ids )
+        watched_ps_ids = list( self._observed_parameter_sets.keys() )
+        psids = self._completed_ps_ids( watched_ps_ids )
         
         for psid in psids:
             ps = ParameterSet.find(psid)
@@ -39,27 +46,50 @@ class OacisWatcher():
             else:
                 print("calling callback for %s" % ps.id().to_s() )
                 executed = True
-                queue = self.observed_parameter_sets[psid]
+                queue = self._observed_parameter_sets[psid]
                 while len(queue) > 0:
                     callback = queue.pop(0)
                     callback( ps )
-                    if self.completed( ps.reload() ) == False:
+                    if self._completed( ps.reload() ) == False:
                         break
 
-        empty_psids = [ psid for psid,callbacks in self.observed_parameter_sets.items() if len(callbacks)==0 ]
+        empty_psids = [ psid for psid,callbacks in self._observed_parameter_sets.items() if len(callbacks)==0 ]
         for empty_psid in empty_psids:
-            self.observed_parameter_sets.pop(empty_psid)
+            self._observed_parameter_sets.pop(empty_psid)
 
         return executed
 
-    def completed_ps_ids(self, watched_ps_ids ):
+    def _check_completed_ps_all(self):
+        from . import ParameterSet
+        from functools import reduce
+        executed = False
+        flattened = reduce(lambda x,y: list(x)+list(y), self._observed_parameter_sets_all.keys(), [])
+        watched_ps_ids = list( set(flattened) )
+        completed = self._completed_ps_ids( watched_ps_ids )
+
+        for psids,callbacks in self._observed_parameter_sets_all.items():
+            if all( (psid in completed) for psid in psids ):
+                print("calling callback for %s" % repr(psids) )
+                executed = True
+                watched_pss = [ ParameterSet.find(psid) for psid in psids ]
+                while len(callbacks) > 0:
+                    callback = callbacks.pop(0)
+                    callback(watched_pss)
+                    if any( self._completed(ps.reload()) for ps in watched_pss ):
+                        break
+
+        empty_keys = [ key for key,callbacks in self._observed_parameter_sets_all.items() if len(callbacks)==0 ]
+        for key in empty_keys:
+            self._observed_parameter_sets_all.pop(key)
+        return executed
+
+    def _completed_ps_ids(self, watched_ps_ids ):
         from . import Run
         query = Run.send('in',parameter_set_id = watched_ps_ids).send('in',status=['created','submitted','running']).selector()
         incomplete_ps_ids = [ psid.to_s() for psid in Run.collection().distinct( "parameter_set_id", query ) ]
         completed = list( set(watched_ps_ids) - set(incomplete_ps_ids) )
-        print( completed )
         return completed
 
-    def completed(self, ps):
+    def _completed(self, ps):
         return ps.runs().send('in', status=['created','submitted','running']).count() == 0
 
--- oacis/oacis_watcher_test.py
+++ oacis/oacis_watcher_test.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import MagicMock
 import os
 
 os.environ['RAILS_ENV'] = 'test'
@@ -15,12 +16,11 @@ class TestOacisWatcher(unittest.TestCase):
     def setUp(self):
         self.DatabaseCleaner = oacis.Rb.const('DatabaseCleaner')
         self.DatabaseCleaner.start()
-        print( oacis.Simulator.count() )
+        assert oacis.Simulator.count() == 0
 
     def tearDown(self):
-        print("being cleaned up")
         self.DatabaseCleaner.clean()
-        print( oacis.Simulator.count() )
+        assert oacis.Simulator.count() == 0
         root_dir = oacis.Rb.const('ResultDirectory').root().to_s()  #=> public/Result_test
         import shutil
         shutil.rmtree( root_dir )
@@ -30,9 +30,8 @@ class TestOacisWatcher(unittest.TestCase):
         pd1 = ParameterDefinition( {'key':"p1", 'type':"Integer", 'default': 0} )
         pd2 = ParameterDefinition( {'key':"p2", 'type':"Float", 'default': 1.0} )
         sim = oacis.Simulator.create( {'name': 'my_test_simulator', 'command': 'echo', 'parameter_definitions': [pd1,pd2]} )
-        #print( repr(sim) )
-        #print( sim.parameter_definitions() )
-        #print( oacis.Simulator.count() )
+        #print( repr(sim), sim.parameter_definitions() )
+        assert oacis.Simulator.count() == 1
         return sim
 
     def build_ps(self, sim, num_ps):
@@ -40,14 +39,137 @@ class TestOacisWatcher(unittest.TestCase):
             v = {"p1": i, "p2": 0.0}
             sim.find_or_create_parameter_set(v)
 
+    def build_run(self, ps, num_created_runs, num_finished_runs):
+        for i in range(num_created_runs):
+            ps.runs().create()
+        for i in range(num_finished_runs):
+            ps.runs().create( status='finished' )
+
     def test_watch_ps(self):
         sim = self.build_simulator()
         self.build_ps(sim, 1)
+        ps = sim.parameter_sets().first()
         w = oacis.OacisWatcher()
-        self.assertEqual( len(w.observed_parameter_sets), 0 )
-        def on_ps_finished(ps): pass
-        w.watch_ps( sim.parameter_sets().first(), on_ps_finished )
-        self.assertEqual( len(w.observed_parameter_sets), 1 )
+        self.assertEqual( len(w._observed_parameter_sets), 0 )
+        w.watch_ps( ps, lambda x: None )
+        self.assertEqual( len(w._observed_parameter_sets), 1 )
+
+        w.watch_ps( ps, lambda x: None )
+        self.assertEqual( len(w._observed_parameter_sets[ps.id().to_s()]), 2 )
+
+    def test_watch_all_ps(self):
+        sim = self.build_simulator()
+        self.build_ps(sim, 2)
+        ps = sim.parameter_sets().first()
+        w = oacis.OacisWatcher()
+
+        self.assertEqual( len(w._observed_parameter_sets_all), 0 )
+        w.watch_all_ps( sim.parameter_sets(), lambda pss: None )
+        self.assertEqual( len(w._observed_parameter_sets_all), 1 )
+        w.watch_all_ps( sim.parameter_sets(), lambda pss: None )
+        psids = tuple( sorted([ps.id().to_s() for ps in sim.parameter_sets()]) )
+        self.assertEqual( len(w._observed_parameter_sets_all[psids]), 2 )
+
+    def test_completed(self):
+        sim = self.build_simulator()
+        self.build_ps(sim, 1)
+        ps = sim.parameter_sets().first()
+        self.build_run(ps, 1, 2)
+        assert ps.runs().count() == 3
+        assert ps.runs().where( status='created' ).count() == 1
+        assert ps.runs().where( status='finished' ).count() == 2
+        w = oacis.OacisWatcher()
+        self.assertFalse( w._completed(ps) )
+
+        ps.runs().where(status='created').first().update_attribute('status','finished')
+        self.assertTrue( w._completed(ps) )
+
+    def test_completed_ps_ids(self):
+        sim = self.build_simulator()
+        self.build_ps(sim, 2)
+        ps1 = sim.parameter_sets().asc('id').first()
+        ps2 = sim.parameter_sets().asc('id').last()
+        self.build_run(ps1, 0, 2)
+        self.build_run(ps2, 1, 1)
+        w = oacis.OacisWatcher()
+        psids = [ps1.id().to_s(), ps2.id().to_s() ]
+        finished = w._completed_ps_ids( psids )
+        self.assertEqual( finished, [ps1.id().to_s()] )
+
+    def test_check_completed_ps__callback(self):
+        sim = self.build_simulator()
+        self.build_ps(sim, 2)
+        ps1 = sim.parameter_sets().asc('id').first()
+        ps2 = sim.parameter_sets().asc('id').last()
+        self.build_run(ps1, 0, 2)
+        self.build_run(ps2, 1, 1)
+
+        mock1 = MagicMock()
+        mock2 = MagicMock()
+
+        w = oacis.OacisWatcher()
+        w.watch_ps(ps1, mock1); w.watch_ps(ps2, mock2)
+        w._check_completed_ps()
+        mock1.assert_called_with(ps1)
+        mock2.assert_not_called()
+
+    def test_check_completed_ps__stop_calling(self):
+        sim = self.build_simulator()
+        self.build_ps(sim, 1)
+        ps = sim.parameter_sets().first()
+        self.build_run(ps, 0, 1)
+
+        mock1 = MagicMock()
+        mock2 = MagicMock()
+        def callback1(ps):
+            mock1();
+            ps.runs().create()
+
+        w = oacis.OacisWatcher()
+        w.watch_ps(ps, callback1 ); w.watch_ps(ps, mock2)
+        ret = w._check_completed_ps()
+        mock1.assert_called_with()
+        mock2.assert_not_called()
+        self.assertTrue( ret )
+
+    def test_check_completed_ps__return_false_when_no_callback_executed(self):
+        sim = self.build_simulator()
+        self.build_ps(sim, 1)
+        ps = sim.parameter_sets().first()
+        self.build_run(ps, 1, 0)
+
+        w = oacis.OacisWatcher()
+        w.watch_ps(ps, lambda x: None )
+        ret = w._check_completed_ps()
+        self.assertFalse( ret )
+
+    def test_check_completed_ps_all__callback(self):
+        sim = self.build_simulator()
+        self.build_ps(sim, 2)
+        for ps in sim.parameter_sets():
+            self.build_run(ps, 0, 2)
+
+        mock1 = MagicMock()
+        w = oacis.OacisWatcher()
+        w.watch_all_ps( sim.parameter_sets(), mock1 )
+        ret = w._check_completed_ps_all()
+        mock1.assert_called_with( sim.parameter_sets().asc('id') )
+        self.assertTrue( ret )
+
+    def test_check_completed_ps_all__not_called(self):
+        sim = self.build_simulator()
+        self.build_ps(sim, 2)
+        ps1 = sim.parameter_sets().asc('id').first()
+        self.build_run( ps1, 0, 2 )
+        ps2 = sim.parameter_sets().asc('id').last()
+        self.build_run( ps2, 1, 1 )
+
+        mock1 = MagicMock()
+        w = oacis.OacisWatcher()
+        w.watch_all_ps( [ps1,ps2], mock1 )
+        ret = w._check_completed_ps_all()
+        mock1.assert_not_called()
+        self.assertFalse( ret )
 
 if __name__ == '__main__':
     setupSuite()
