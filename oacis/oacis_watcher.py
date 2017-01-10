47a53b213c290119ea4f7ee6afec11023550308a
Yohsuke Murase <yohsuke.murase@gmail.com>
2017-01-10 11:24:55 +0900

    avoid adding logger.handler twice

--- oacis/oacis_watcher.py
+++ oacis/oacis_watcher.py
@@ -58,11 +58,12 @@ class OacisWatcher():
         logger = logging.getLogger(__name__)
         logger.setLevel( logging.INFO )
         logger.propagate = False
-        ch = logging.StreamHandler()
-        ch.setLevel( logging.INFO )
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        ch.setFormatter(formatter)
-        logger.addHandler(ch)
+        if not logger.handlers:
+            ch = logging.StreamHandler()
+            ch.setLevel( logging.INFO )
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            ch.setFormatter(formatter)
+            logger.addHandler(ch)
         return logger
 
     def _check_completed_ps(self):
