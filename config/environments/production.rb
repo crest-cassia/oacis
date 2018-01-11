3ed1e93a2c1a5d56ca8804ffeea8119b68e3e909
Yohsuke Murase <yohsuke.murase@gmail.com>
2018-01-11 23:05:11 +0900

    disable AR config and add Mongoid configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # config.active_record.dump_schema_after_migration = false
 
   # user code  ----------------
   class LoggerFormatWithTime
