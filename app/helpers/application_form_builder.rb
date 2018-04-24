0b0a3c2644483d103535c560e0c69631c1e75ff4
Yohsuke Murase <yohsuke.murase@gmail.com>
2018-04-24 18:09:16 +0900

    refactoring tooltip

--- app/helpers/application_form_builder.rb
+++ app/helpers/application_form_builder.rb
@@ -1,11 +1,5 @@
 class ApplicationFormBuilder < ActionView::Helpers::FormBuilder
-  def label_with_tooltip(attribute, text, *json_path)
-    json = TOOLTIP_DESCS
-    json = json.dig(*json_path)
-    if json.present? && json.is_a?(String)
-      label(attribute, text, class: 'col-md-2 control-label',  data: {html: 'true', toggle: 'tooltip'}, title: "#{json}")
-    else
-      label(attribute, text, class: 'col-md-2 control-label')
-    end
+  def label_c(attribute, text=nil)
+    label(attribute, text, class: 'col-md-2 control-label')
   end
 end
