84b524a6d0c80fefb5ad7e9d9f199c99c5bd0f29
Yohsuke Murase <yohsuke.murase@gmail.com>
2018-04-24 15:08:54 +0900

    refactoring: remove redundant trigger of tooltips

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -2,3 +2,6 @@ jQuery(function() {
   $("a[rel~=popover], .has-popover").popover();
   $("a[rel~=tooltip], .has-tooltip").tooltip();
 });
+jQuery(function() {
+  $('[data-toggle="tooltip"]').tooltip()
+});
