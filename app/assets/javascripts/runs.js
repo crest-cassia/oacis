3b192e82f0a833fbd663b1648e6579e8cf158dab
Yohsuke Murase <yohsuke.murase@gmail.com>
2018-04-30 16:28:32 +0900

    show multiple tables in Jobs page

--- app/assets/javascripts/runs.js
+++ app/assets/javascripts/runs.js
@@ -15,29 +15,34 @@ function datatables_for_runs_table(selector) {
       $(row).attr('id', lnId);
     }
   });
-  const actionUrl = '/runs/_delete_selected';
-  $(selector+'_length').css('height', '45px');
-  $(selector+'_length').append(
+
+  // reload settings
+  const wrapperDiv = $(selector).closest(selector+'_wrapper');
+  const lengthDiv = wrapperDiv.find(selector+'_length');
+  lengthDiv.css('height', '45px');
+  lengthDiv.append(
     '<i class="fa fa-refresh padding-half-em reload_icon clickable" id="runs_list_refresh"></i>' +
     '<div class="auto_reload_setting">' +
     '<label class="form-check-label clickable" for="runs_list_refresh_cb">auto reload<input type="checkbox" class="form-check-input" id="runs_list_refresh_cb" /></label>' +
     '<label for="runs_list_refresh_tb"><input type="text" pattern="^[0-9]*$" class="form-control form-control-sm" id="runs_list_refresh_tb" size="10">sec</label>' +
     '</div>'
   );
-  $(selector+'_length').parent().after(
+  const refresh_icon = $(selector+'_length').children('#runs_list_refresh');
+  refresh_icon.on('click', function() { oTable.ajax.reload(null, false);});
+
+  // checkbox settings
+  const actionUrl = '/runs/_delete_selected';
+  lengthDiv.parent().after(
     '<div class="dataTables_length" id="selected_runs_ctl_div" style="height: 43px; clear: both; padding-left: 15px;">' +
     '<form name="runs_form" id="runs_select_form" action="' + actionUrl + '" method="post">' +
     '<input type="hidden" name="authenticity_token" value="' + $('meta[name="csrf-token"]').attr('content') + '">' +
-    '<span class="add-margin-top pull-left add-padding-right">Selected <span id="runs_count"></span> Runs</span>' +
+    '<span class="add-margin-top pull-left add-padding-right">Selected <span id="runs_count">0</span> Runs</span>' +
     '<input type="hidden" name="id_list" id="run_selected_id_list">' +
     '<input type="button" class="btn btn-warning margin-half-em" value="Delete" id="runs_delete_sel">' +
     '</form>' +
     '</div>'
   );
-  $('#runs_count').text('0');
-  var refresh_icon = $(selector+'_length').children('#runs_list_refresh');
-  refresh_icon.on('click', function() { oTable.ajax.reload(null, false);});
-  $('#run_check_all').on('change', function() {
+  wrapperDiv.find('#run_check_all').on('change', function() {
     const checkAll = $('#run_check_all').prop('checked');
     if(checkAll) {
       $('input[name="checkbox[run]"]').prop('checked', true).trigger('change');
@@ -45,32 +50,32 @@ function datatables_for_runs_table(selector) {
       $('input[name="checkbox[run]"]').prop('checked', false).trigger('change');
     }
   });
-  $(document).on('change','input[name="checkbox[run]"]', function() {
+  wrapperDiv.on('change','input[name="checkbox[run]"]', function() {
     let checkedCnt = 0;
-    const numCheckBoxes = $('input[name="checkbox[run]"]').length;
-    const idList = $('.dataTable tbody input:checked').map(function() {
+    const numCheckBoxes = wrapperDiv.find('input[name="checkbox[run]"]').length;
+    const idList = wrapperDiv.find('.dataTable tbody input:checked').map(function() {
       checkedCnt += 1;
       return $(this).val();
     }).get();
-    $('#runs_count').text(checkedCnt);
-    $('#run_selected_id_list').val(idList);
+    wrapperDiv.find('#runs_count').text(checkedCnt);
+    wrapperDiv.find('#run_selected_id_list').val(idList);
     setSelectRunsCtlDivDisp(checkedCnt > 0);
-    $('#run_check_all').prop('checked', checkedCnt == numCheckBoxes);
+    wrapperDiv.find('#run_check_all').prop('checked', checkedCnt == numCheckBoxes);
   });
-  $('#runs_delete_sel').on('click', function() {
+  wrapperDiv.find('#runs_delete_sel').on('click', function() {
     const ret = confirm('Delete selected Runs. Are you sure?');
     if (ret) {
-      $('#runs_select_form').submit();
+      wrapperDiv.find('#runs_select_form').submit();
     }
   });
   let setSelectRunsCtlDivDisp = (dispFlag) => {
     if (dispFlag) {
-      $('#selected_runs_ctl_div').show();
-      $(selector+'_length').hide();
+      wrapperDiv.find('#selected_runs_ctl_div').show();
+      lengthDiv.hide();
     }
     else {
-      $('#selected_runs_ctl_div').hide();
-      $(selector+'_length').show();
+      wrapperDiv.find('#selected_runs_ctl_div').hide();
+      lengthDiv.show();
     }
   }
   setSelectRunsCtlDivDisp(false);
