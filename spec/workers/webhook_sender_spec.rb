c3df342351f6b4ee31b3aa673356dd933824ab45
takeshi-uchitane <t.uchitane@gmail.com>
2020-01-24 20:54:07 +0900

    refactoring webhook_sender_spec

--- spec/workers/webhook_sender_spec.rb
+++ spec/workers/webhook_sender_spec.rb
@@ -8,11 +8,19 @@ describe WebhookSender do
       @sim = FactoryBot.create(:simulator, parameter_sets_count: 2, runs_count: 0)
       Webhook.create() if Webhook.count == 0
       @webhook = Webhook.first
-      @webhook_mock = instance_double(Webhook)
+      @http_mock = instance_double(Net::HTTP)
       allow(Net::HTTP).to receive(:new).with(anything(), anything()).and_return(@http_mock)
       allow(@http_mock).to receive(:use_ssl=).with(anything()).and_return("Anything")
       allow(@http_mock).to receive(:request).with(anything()).and_return("Success")
       @logger = Logger.new( File.open('/dev/null','w') )
+    end
+
+    it "sends webhook" do
+      @sim.parameter_sets.each do |ps|
+        ps.runs.create!(status: :finished, submitted_to: Host.first)
+      end
+      WebhookSender.perform(@logger)
+      expect(Net::HTTP).to have_received(:new).once
     end
 
     it "do nothing if there is no 'enabled' webhook" do
