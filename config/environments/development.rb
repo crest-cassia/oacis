cadf9842acfccf81da1a9d53e6f48e04c04ffe3c
Yohsuke Murase <yohsuke.murase@gmail.com>
2022-06-03 16:14:59 +0900

    change file_watcher setting for M1 mac

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,7 +48,9 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # to run OACIS docker image on M1 mac, it must not be `EventedFileUpdateChecker`
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 
   # user code -----------
   class LoggerFormatWithTime
